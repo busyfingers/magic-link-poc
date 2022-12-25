865761a8b21ea7b14122e995174409532854c38c
busyfingers <n.malmqvist@gmail.com>
2022-12-25 20:25:15 +0100

    Cleanup

--- pages/api/auth/magicLink.ts
+++ pages/api/auth/magicLink.ts
@@ -1,7 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
-import config from "../../../lib/config";
 
 type Response = {
   link: string;
--- pages/login.tsx
+++ pages/login.tsx
@@ -2,9 +2,6 @@ import { useEffect, useState } from "react";
 import { signIn } from "next-auth/react";
 
 export default function Login() {
-  const [loading, setLoading] = useState<Boolean>(true);
-  const [status, setStatus] = useState<string>();
-
   useEffect(() => {
     const url = new URL(window.location.href);
 
@@ -14,19 +11,9 @@ export default function Login() {
       signIn("credentials", {
         token,
         callbackUrl: "/",
-      })
-        .then((res) => {
-          setStatus("Login ok!");
-        })
-        .catch((err) => {
-          console.log(err.message);
-          setStatus("neas");
-        })
-        .finally(() => {
-          setLoading(false);
-        });
+      });
     }
   }, []);
 
-  return <>{loading ? <h2>Loggar in...</h2> : <h2>{status}</h2>}</>;
+  return <>{<h2>Loggar in...</h2>}</>;
 }
